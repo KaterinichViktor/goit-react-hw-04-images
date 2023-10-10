23f42d7e2a2ef17756a1c34cae616133f667cf93
KaterinichViktor <107800265+KaterinichViktor@users.noreply.github.com>
2023-10-10 17:11:56 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,107 +1,122 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Audio } from 'react-loader-spinner';
 
 import Searchbar from './Searchbar';
 import ImageGallery from './ImageGallery';
-import Button from './Button';
-import Modal from './Modal';
+import Button from './Button'; 
+import Modal from './Modal'; 
 
-import { fetchImages } from '../API';
+import { fetchImages } from '../API'; 
 
 import '../App.css';
 
-const App = () => {
-  const [images, setImages] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const [query, setQuery] = useState('');
-  const [showModal, setShowModal] = useState(false);
-  const [largeImageURL, setLargeImageURL] = useState('');
-  const [page, setPage] = useState(1);
+class App extends Component {
+  state = {
+    images: [],
+    isLoading: false,
+    error: null,
+    query: '',
+    showModal: false,
+    largeImageURL: '',
+    page: 1, 
+  };
 
-  useEffect(() => {
-    if (query !== '') {
-      setPage(1);
-      fetchImages(query, 1)
-        .then((fetchedImages) => {
-          if (fetchedImages.length === 0) {
-            toast.error(`No images found for ${query}`);
-          }
-          setImages(fetchedImages);
-          setPage((prevPage) => prevPage + 1);
-        })
-        .catch((fetchError) => setError(fetchError))
-        .finally(() => setIsLoading(false));
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.query !== this.state.query) {
+
+      this.setState({ page: 1 }, () => {
+        this.fetchImages();
+      });
     }
-  }, [query]);
+  }
 
-  const handleFormSubmit = (newQuery) => {
-    setQuery(newQuery);
-    setImages([]);
-    setError(null);
-    setPage(1);
+  handleFormSubmit = query => {
+    this.setState({ query, images: [], error: null, page: 1 });
   };
 
-  const fetchMoreImages = () => {
-    setIsLoading(true);
+  fetchImages = () => {
+    const { query, page } = this.state;
+    this.setState({ isLoading: true });
+
     fetchImages(query, page)
-      .then((fetchedImages) => {
-        setImages((prevImages) => [...prevImages, ...fetchedImages]);
-        setPage((prevPage) => prevPage + 1);
+      .then(images => {
+        if (images.length === 0) {
+          toast.error(`No images found for ${query}`);
+        }
+        this.setState(prevState => ({
+          images: [...prevState.images, ...images],
+          page: prevState.page + 1,
+        }));
       })
-      .catch((fetchError) => setError(fetchError))
-      .finally(() => setIsLoading(false));
+      .catch(error => this.setState({ error }))
+      .finally(() => {
+        this.setState({ isLoading: false });
+        window.scrollTo({
+          top: document.documentElement.scrollHeight,
+          behavior: 'smooth',
+        });
+      });
   };
 
-  const openModal = (largeImageUrl) => {
-    setShowModal(true);
-    setLargeImageURL(largeImageUrl);
+  openModal = (largeImageURL) => {
+    console.log('Large Image URL:', largeImageURL);
+    this.setState({ showModal: true, largeImageURL });
   };
 
-  const closeModal = () => {
-    setShowModal(false);
-    setLargeImageURL('');
+  closeModal = () => {
+    this.setState({ showModal: false, largeImageURL: '' });
   };
 
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        fontSize: 24,
-      }}
-    >
-      <Searchbar onSubmit={handleFormSubmit} />
-
-      {error && <p>Whoops, something went wrong: {error.message}</p>}
-
-      <ImageGallery images={images} onImageClick={openModal} />
-
-      {isLoading && (
-        <Audio
-          height={80}
-          width={80}
-          radius={9}
-          color="green"
-          ariaLabel="three-dots-loading"
-          wrapperStyle=""
-          wrapperClass=""
-        />
-      )}
-
-      {images.length > 0 && !isLoading && <Button onClick={fetchMoreImages} />}
-
-      {showModal && (
-        <Modal src={largeImageURL} alt="Large Image" onClose={closeModal} />
-      )}
-
-      <ToastContainer autoClose={3000} position="top-right" />
-    </div>
-  );
-};
+  render() {
+    const { images, isLoading, error, showModal, largeImageURL } = this.state;
+    const showLoadMoreButton = images.length >= 12;
+  
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          fontSize: 24,
+        }}
+      >
+        <Searchbar onSubmit={this.handleFormSubmit} />
+  
+        {error && <p>Whoops, something went wrong: {error.message}</p>}
+  
+        <ImageGallery images={images} onImageClick={this.openModal} />
+  
+        {isLoading && (
+          <Audio
+            height={80}
+            width={80}
+            radius={9}
+            color="green"
+            ariaLabel="three-dots-loading"
+            wrapperStyle=""
+            wrapperClass=""
+          />
+        )}
+  
+        {showLoadMoreButton && !isLoading && (
+          <Button onClick={this.fetchImages} />
+        )}
+  
+        {showModal && (
+          <Modal
+            src={largeImageURL}
+            alt="Large Image"
+            onClose={this.closeModal}
+          />
+        )}
+  
+        <ToastContainer autoClose={3000} position="top-right" />
+      </div>
+    );
+  }  
+}
 
 export default App;
